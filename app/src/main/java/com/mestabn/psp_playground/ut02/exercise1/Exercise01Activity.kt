bb16208459a73d3c235ad3dca6313bd0dd713870
mesteban10 <mariaesteban1997@gmail.com>
2021-10-25 17:52:07 +0200

    Creamos Actividad para el ejercicio y la hacemos principal en el AndroidManifest

---
+++ app/src/main/java/com/mestabn/psp_playground/ut02/exercise1/Exercise01Activity.kt
@@ -0,0 +1,12 @@
+package com.mestabn.psp_playground.ut02.exercise1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mestabn.psp_playground.R
+
+class Exercise01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise01)
+    }
+}
